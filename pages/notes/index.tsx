8f377a87a08803ff7a8bdf1dc961a45a599a9170
Serge Basangovs <serge@visfo.health>
2022-04-01 11:49:52 +0100

    notes: client side fetching

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_ALL_NOTES } from '../../src/apollo/queries'
+import { Note } from '../../src/types'
 import Page from '../../src/containers/Page'
 
 const Notes = () => {
+  const { data, loading, error } = useQuery<{ getNotes: Note[] }>(GET_ALL_NOTES)
+
+  const renderResults = () => {
+    if (loading) return <p>Loading notes...</p>
+    if (error) return <p>Error: {error.message}</p>
+    if (!!data && data.getNotes.length === 0) return <p>no notes</p>
+    return data!.getNotes.map((note) => (
+      <div key={note.id}>
+        <h3>{note.title}</h3>
+      </div>
+    ))
+  }
   return (
     <Page>
       <h1 sx={{ fontSize: 8, my: 0 }}>Notes</h1>
+      <div>{renderResults()}</div>
     </Page>
   )
 }
